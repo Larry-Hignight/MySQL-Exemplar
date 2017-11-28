7b7f21330121a7ea96d9cd695fca4f21980982a9
Larry-Hignight <larry.hignight@gmail.com>
2017-11-28 08:31:55 +0000

    Added a small utility script to automate the normal Docker steps of build, run then exec.

---
+++ build-and-run.sh
@@ -0,0 +1,14 @@
+echo "Building the database image"
+sudo docker build --rm -t db-exemplar .
+
+echo "Starting the database"
+sudo docker run --rm -e MYSQL_ROOT_PASSWORD=12345678 --name db -d db-exemplar
+
+echo "Pausing to allow the database to finish starting up"
+sleep 10
+
+echo "Attempting to connect to the database"
+sudo docker exec -it db mysql -u root -p
+
+# When you need to inspect the data files... 
+# sudo docker exec -it relaxed_curie bash cd /var/lib/mysql-files
