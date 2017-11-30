0cd97414183bd5e491eb91525fc76c2fed1c8258
Larry-Hignight <larry.hignight@gmail.com>
2017-11-30 06:27:32 +0000

    Replaced the NBA csv file with a sql load script.

---
+++ init/538-database.sql
@@ -0,0 +1,21 @@
+create database nba_2017;
+use nba_2017;
+
+CREATE TABLE IF NOT EXISTS game_results (
+  game_date DATE,
+  abbv1 VARCHAR(20),
+  visitor VARCHAR(20),
+  points1 VARCHAR(20),     -- This is the point spread and isn't populated
+  win_prob1 FLOAT,
+  score1 INT,
+  abbv2 VARCHAR(20),
+  home VARCHAR(20),
+  points2 FLOAT,
+  win_prob2 FLOAT,
+  score2 INT
+);
+
+-- LOAD DATA INFILE '/var/lib/mysql-files/538-nba-game-predictions.csv'
+-- INTO TABLE game_results
+-- FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+-- LINES TERMINATED BY '\r\n';
