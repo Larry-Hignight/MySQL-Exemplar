4ccb8db17510f6bab763278d3241e4e76d3ae44d
Larry-Hignight <larry.hignight@gmail.com>
2017-11-30 06:49:41 +0000

    Deleted trailing whitespace from all of the files.

--- python/database-script.py
+++ python/database-script.py
@@ -21,11 +21,11 @@ cnx.close()
 ## -- Example using SQLAlchemy -- ##
 # import sqlalchemy
 # sqlalchemy.__version__
-# 
+#
 # from sqlalchemy import create_engine
 # engine = create_engine('mysql+mysqlconnector://user:12345678@db/library')
 # con = engine.connect()
-# 
+#
 # # Create tables for users and addresses
 # from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 # metadata = MetaData()
@@ -34,22 +34,22 @@ cnx.close()
 #   Column('name', String(30)),
 #   Column('fullname', String(60)),
 # )
-# 
+#
 # addresses = Table('addresses', metadata,
 #   Column('id', Integer, primary_key=True),
 #   Column('user_id', None, ForeignKey('users.id')),
 #   Column('email_address', String(100), nullable=False)
 # )
-# 
+#
 # metadata.create_all(engine)
-# 
-# 
+#
+#
 # ## -- What's next??? -- ##
-# 
+#
 # # Insert values for all columns
 # ins = users.insert()
 # str(ins)
-# 
+#
 # # Insert values for specific columns
 # ins = users.insert().values(name='jack', fullname='Jack Jones')
 # str(ins)
@@ -58,13 +58,12 @@ cnx.close()
 # ins.compile().params
 # result = con.execute(ins)
 # result.inserted_primary_key
-# 
-# 
+#
+#
 # # Select...
-# 
+#
 # from sqlalchemy.sql import select
 # s = select([users])
 # result = con.execute(s)
-# 
+#
 # for row in result:  print(row)
-
